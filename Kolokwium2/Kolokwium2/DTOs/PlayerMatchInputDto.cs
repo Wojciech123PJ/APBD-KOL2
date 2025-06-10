786def441a2f07d00210cb7fd80bfd0ae84e99d3
Wojciech Kaminski <s30525@pjwstk.edu.pl>
2025-06-10 15:30:35 +0200

    Fix

--- Kolokwium2/Kolokwium2/DTOs/PlayerMatchInputDto.cs
+++ Kolokwium2/Kolokwium2/DTOs/PlayerMatchInputDto.cs
@@ -1,16 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Kolokwium2.Data;
 
 public class PlayerMatchInputDto
 {
+    [Required]
     public string FirstName { get; set; } = string.Empty;
+    [Required]
     public string LastName { get; set; } = string.Empty;
+    [Required]
     public DateTime BirthDate { get; set; }
+    [Required]
     public ICollection<MatchDto> Matches { get; set; }
 }
 
 public class MatchDto
 {
+    [Required]
     public int MatchId { get; set; }
+    [Required]
     public int Mvps { get; set; }
+    [Required]
     public decimal Rating { get; set; }
 }
